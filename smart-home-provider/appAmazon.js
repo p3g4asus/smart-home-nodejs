64c4196370d6312a93dcd6d91bdad81583e5eadf
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2018-04-24 01:35:49 +0200

    Ancora miglioramenti allo script amazon

--- smart-home-provider/appAmazon.js
+++ smart-home-provider/appAmazon.js
@@ -1,3 +1,4 @@
 __isLocal = 1;
-__smartHomeProviderCloudEndpoint = "https://mfzaws1.ddns.net"
+__smartHomeProviderCloudEndpoint = "https://mfzaws1.ddns.net";
+process.chdir(__dirname);
 require('./cloud/smart-home-provider-cloud.js');
