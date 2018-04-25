5369ccb8367b12965627f7d22c8a2a713ccfe16d
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2018-04-25 18:18:08 +0200

    Corretto errore script amazon

--- smart-home-provider/appAmazon.js
+++ smart-home-provider/appAmazon.js
@@ -1,4 +1,4 @@
-__flag = Config.setInside(0,"START_TYPE","LOCAL","AUTO_DEV","YES","RESET_DEV","NO","WELL_KNOWN","YES","AUTOLOGIN","YES");
+__flag = require('./cloud/config-provider.js').setInside(0,"START_TYPE","LOCAL","AUTO_DEV","YES","RESET_DEV","NO","WELL_KNOWN","YES","AUTOLOGIN","YES");
 __smartHomeProviderCloudEndpoint = "https://mfzaws1.ddns.net";
 process.chdir(__dirname);
 require('./cloud/smart-home-provider-cloud.js');
