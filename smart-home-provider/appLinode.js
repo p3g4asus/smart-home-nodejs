c6a6b8c8f21794c147e57e6d488d7089ccad2ce0
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2018-06-10 10:55:36 +0200

    Corretto script di  avvio linode

--- smart-home-provider/appLinode.js
+++ smart-home-provider/appLinode.js
@@ -1,3 +1,3 @@
-__argv = ["smart-home=https://mfzlinode.ddns.net","-fSTART_TYPE=LOCAL","-fAUTO_DEV=YES","-fRESET_DEV=NO","-fWELL_KNOWN=YES","-fAUTOLOGIN=YES"];
+__argv = ["username=fulminedipegasus","smart-home=https://mfzlinode.ddns.net","-fSTART_TYPE=LOCAL","-fAUTO_DEV=YES","-fRESET_DEV=NO","-fWELL_KNOWN=YES","-fAUTOLOGIN=YES"];
 process.chdir(__dirname);
 require('./cloud/smart-home-provider-cloud.js');
