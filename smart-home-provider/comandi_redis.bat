c516b0e51e6c559e22e73fed74a3304d41bcf70b
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2018-06-24 11:11:18 +0200

    aggiunti comandi andrea

--- smart-home-provider/comandi_redis.bat
+++ smart-home-provider/comandi_redis.bat
@@ -1,12 +1,15 @@
 hmset client:ZxjqWpsYj3 username fulminedipegasus stringid ZxjqWpsYj3 secret hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv apikey AIzaSyBNZ0MwFCCjPOiB-Zt0NBancTpE5slwQqs
 hmset client:RKkWfsi0Z9 username p3g4asus stringid RKkWfsi0Z9 secret eToBzeBT7OwrPQO8mZHsZtLp1qhQbe apikey AIzaSyCLfX3-gk7iKpXibMPka4ruCQbmqyLALUk
 hmset client:vBg68IP6FA username rsp stringid vBg68IP6FA secret nM4YNDaP91TCxa41QlovDFqwz81G0w apikey AIzaSyBuvpCnZ7XLjY3zownTeschB0lhWfGDqe0
+hmset client:s9HqYD53lP username andrea stringid s9HqYD53lP secret aJu40HvbPqwR54HjzDt92T5KmdPatr apikey AIzaSyCtSFwWXDqZNO3d10dwxSF-kQEspR36sgU
 sadd client:validclients ZxjqWpsYj3
 sadd client:validclients RKkWfsi0Z9
 sadd client:validclients vBg68IP6FA
+sadd client:validclients s9HqYD53lP
 set client:username:fulminedipegasus ZxjqWpsYj3
 set client:username:p3g4asus RKkWfsi0Z9
 set client:username:rsp vBg68IP6FA
+set client:username:andrea s9HqYD53lP
 sadd translations it
 sadd translations en
 hmset translation:it "right" "destra" "subtitle" "sottotitolo" "power" "accendi" "mute" "muto" "ffw" "avanti veloce" "rec" "registra" "v+" "alza volume" "ttx" "televideo" "back" "indietro" "yellow" "tasto giallo" "v-" "abbassa volume" "down" "basso" "exit" "esci" "sky" "murdock" "p+" "canale successivo" "return" "ritorno" "tools" "strumenti" "blue" "tasto blu" "pause" "pausa" "manual" "manuale" "up" "su" "chlist" "lista canali" "green" "tasto verde" "revw" "indietro veloce" "esc" "esci" "p-" "canale precedente" "guide" "guida" "red" "tasto rosso" "s201" "spina" "magiccube" "cubo" "blackbeam1" "fagiolo" "av" "sorgente" "orvibo1" "cupola" "2sky" "satellite" "2tv" "televisione" "remote" "telecomando" "chan" "orso"
